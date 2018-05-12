aa036001482a03fe096c2e551aec104730b1c302
zbig1000 <zbig.lupina@gmail.com>
2018-05-12 12:25:58 +0200

    Codility - Dices refactored

--- SmallApps/src/ArrayTests/Dices.java
+++ SmallApps/src/ArrayTests/Dices.java
@@ -2,18 +2,21 @@ package ArrayTests;
 
 public class Dices {
     //finding minimal amount of turns of dices to turn all to same side
+    private final int[] sides1 = {1, 2, 6, 5};
+    private final int[] sides2 = {3, 2, 4, 5};
+
     public int solution(int[] A) {
         int minMoves = 0;
         for (int i = 1; i < 7; i++) {
             int moves = 0;
             int notFound = 0;
             for (int j = 0; j < A.length; j++) {
-                int currentMinimalMove =  findMinimalMoveFromTo(A[j], i);
-                if (currentMinimalMove>=0) moves += currentMinimalMove;
-                    else notFound = -1;
+                int currentMinimalMove = findMinimalMoveFromTo(A[j], i);
+                if (currentMinimalMove >= 0) moves += currentMinimalMove;
+                else notFound = -1;
             }
-            if ((minMoves == 0) && (moves!=0) && (notFound==0)) minMoves = moves;
-                else minMoves = ((moves != 0) && (notFound==0)) ? (Math.min(minMoves, moves)) : (minMoves);
+            if ((minMoves == 0) && (moves != 0) && (notFound == 0)) minMoves = moves;
+            else minMoves = ((moves != 0) && (notFound == 0)) ? (Math.min(minMoves, moves)) : (minMoves);
         }
         return minMoves;
     }
@@ -23,64 +26,45 @@ public class Dices {
     }
 
     private int movingRight(int pipStart, int pipTarget) {
-        int[] sides1 = {1, 2, 6, 5};
-        int[] sides2 = {3, 2, 4, 5};
-
         int movesRight1 = -1;
         int movesRight2 = -1;
+
         int position = findInSide(pipStart, sides1);
-        if (position != -1) {
-            for (int i = 0; i < 4; i++) {
-                if (sides1[(position + i) % 4] == pipTarget) {
-                    movesRight1 = i;
-                    break;
-                }
-            }
-        }
+        if (position != -1) movesRight1 = findNumerMovesRight(sides1, position, pipTarget);
 
         position = findInSide(pipStart, sides2);
-        if (position != -1) {
-            for (int i = 0; i < 4; i++) {
-                if (sides2[(position + i) % 4] == pipTarget) {
-                    movesRight2 = i;
-                    break;
-                }
-            }
-        }
+        if (position != -1) movesRight2 = findNumerMovesRight(sides2, position, pipTarget);
 
         if (movesRight1 < 0) return movesRight2;
         if (movesRight2 < 0) return movesRight1;
         return Math.min(movesRight1, movesRight2);
     }
 
-    private int movingLeft(int pipStart, int pipTarget) {
-        int[] sides1 = {1, 2, 6, 5};
-        int[] sides2 = {3, 2, 4, 5};
+    private int findNumerMovesRight(int[] side, int currentPosition, int pip) {
+        for (int i = 0; i < 4; i++) {
+            if (side[(currentPosition + i) % 4] == pip) return i;
+        }
+        return -1;
+    }
 
+    private int findNumerMovesLeft(int[] side, int currentPosition, int pip) {
+        int newPosition;
+        for (int i = 0; i < 4; i++) {
+            newPosition = ((currentPosition - i) >= 0) ? (currentPosition - i) : (4 + currentPosition - i);
+            if (side[(newPosition)] == pip) return i;
+        }
+        return -1;
+    }
+
+    private int movingLeft(int pipStart, int pipTarget) {
         int movesLeft1 = -1;
         int movesLeft2 = -1;
-        int currentPosition = 0;
+
         int position = findInSide(pipStart, sides1);
-        if (position != -1) {
-            for (int i = 0; i < 4; i++) {
-                currentPosition = ((position - i) >= 0) ? (position - i) : (4 + position - i);
-                if (sides1[currentPosition] == pipTarget) {
-                    movesLeft1 = i;
-                    break;
-                }
-             }
-        }
+        if (position != -1) movesLeft1 = findNumerMovesLeft(sides1, position, pipTarget);
 
         position = findInSide(pipStart, sides2);
-        if (position != -1) {
-            for (int i = 0; i < 4; i++) {
-                currentPosition = ((position - i) >= 0) ? (position - i) : (4 + position - i);
-                if (sides2[currentPosition] == pipTarget) {
-                    movesLeft2 = i;
-                    break;
-                }
-            }
-        }
+        if (position != -1) movesLeft2 = findNumerMovesLeft(sides2, position, pipTarget);
 
         if (movesLeft1 < 0) return movesLeft2;
         if (movesLeft2 < 0) return movesLeft1;
@@ -96,8 +80,6 @@ public class Dices {
 
     public static void main(String[] args) {
         int[] A = {1, 6, 2, 3};
-
         System.out.println(new Dices().solution(A));
     }
-
 }
